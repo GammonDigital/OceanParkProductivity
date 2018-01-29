42595bdee5d6b0362dc117030a7c657c37451fd1
JT <streamlinegammon+jt@gmail.com>
2018-01-29 20:48:43 +0800

    changed from using gspread to pygsheets

--- OceanParkProductivity.py
+++ OceanParkProductivity.py
@@ -4,8 +4,10 @@ Note that Google Sheet has cell limits
 
 from bluepy.btle import Scanner, DefaultDelegate
 import datetime
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import pygsheets
+import csv
+# import gspread
+# from oauth2client.service_account import ServiceAccountCredentials
 
 # Variables
 with open("/home/pi/Documents/Python/OceanParkProductivity/beaconReg.csv", "r") as fBeacon:  # Import registered beacons
@@ -20,10 +22,15 @@ with open("/home/pi/Documents/Python/OceanParkProductivity/scannerId.txt", "r") 
 
 
 # gspread access spreadsheet
-scope = ['https://spreadsheets.google.com/feeds']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/pi/Documents/Python/OceanParkProductivity/drive_client_secret.json', scope)
-gc = gspread.authorize(credentials)
-googlesheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1RvO3S0LzEts_X72-xVm2_SvPcJBnQUsdhH1YoKSimNo").sheet1
+# scope = ['https://spreadsheets.google.com/feeds']
+# credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/pi/Documents/Python/OceanParkProductivity/drive_client_secret.json', scope)
+# gc = gspread.authorize(credentials)
+# googlesheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1RvO3S0LzEts_X72-xVm2_SvPcJBnQUsdhH1YoKSimNo").sheet1
+
+# pygsheets access spreadsheet
+gc = pygsheets.authorize(outh_file='/home/pi/Documents/Python/OceanParkProductivity/client_secret.json')
+sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1RvO3S0LzEts_X72-xVm2_SvPcJBnQUsdhH1YoKSimNo")
+wks  = sh.sheet1
 
 
 # BLE Scan
@@ -33,26 +40,27 @@ class ScanDelegate(DefaultDelegate):
         DefaultDelegate.__init__(self)
 
 
-def main():
-    # Check device proximity
-    while True:
-        time = datetime.datetime.now()
-        scanner = Scanner().withDelegate(ScanDelegate())
-        devices = scanner.scan(60)  # Scans for n seconds, note that the Minew beacons broadcasts every 2 seconds
-        scanSummary = []
-        for dev in devices:
-            if (dev.addr in beaconAddr) and (dev.rssi > beaconThres):
-                scanAddr = list(item[0] for item in scanSummary)
-                if dev.addr not in [item[0] for item in scanSummary]:
-                    scanSummary.append([dev.addr, dev.rssi])
-                else:
-                    rownum = scanAddr.index(dev.addr):
-                    if dev.rssi > scanSummary[rownum][1]:
-                        scanSummary[rownum][1] = dev.rssi
-        if len(scanSummary) > 0:
-            for eachRow in scanSummary:
-                googlesheet.append_row([scannerId, "{:%Y-%m-%d %H:%M}".format(datetime.datetime.now()), eachRow[0], eachRow[1]])
+# Check device proximity
+while True:
+    time = datetime.datetime.now()
+    scanner = Scanner().withDelegate(ScanDelegate())
+    devices = scanner.scan(10)  # Scans for n seconds, note that the Minew beacons broadcasts every 2 seconds
+    scanSummary = []
+    for dev in devices:
+        if (dev.addr in beaconAddr) and (dev.rssi > beaconThres):
+            scanAddr = list(item[0] for item in scanSummary)
+            if dev.addr not in [item[0] for item in scanSummary]:
+                scanSummary.append([dev.addr, dev.rssi])
+                print("Found {} at {}".format(dev.addr, dev.rssi)) #debugging only
+            else:
+                rownum = scanAddr.index(dev.addr)
+                if dev.rssi > scanSummary[rownum][1]:
+                    scanSummary[rownum][1] = dev.rssi
+                    print("Amended device rssi to {}".format([dev.rssi])) #debugging only
+    if len(scanSummary) > 0:
+        for eachRow in scanSummary:
+            # timestamp = "{:%Y-%m-%d %H:%M}".format(datetime.datetime.now())
+            wks.append_table(values=[scannerId, "{:%Y-%m-%d %H:%M:%S}".format(time), eachRow[0], eachRow[1]])
+            print("Add to Google Sheets {} {} {} {}".format(scannerId, time, eachRow[0], eachRow[1]))
+            # googlesheet.append_row([scannerId, "{:%Y-%m-%d %H:%M}".format(datetime.datetime.now()), eachRow[0], eachRow[1]])
 
-
-if __name__ == '__main__':
-    main()
